e7806a5c928956e0c5a7253dcac3c13da79588b5
Prantik <prantiknoor@gmail.com>
2023-05-05 11:09:50 +0600

    initial setup done - player-rest-api

---
+++ player-rest-api/app.js
@@ -0,0 +1,10 @@
+/**
+* Player Microservice
+* CRUD  - Create Read Update Delete
+* GET   - find all players
+* POST  - / - create a new player and save into db
+* GET   - /:id - find a single player by id
+* PUT   - /:id - update or create player
+* PATCH - /:id - update player
+* DELETE- /:id - delete player from db
+*/
