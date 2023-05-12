6b26e3b5c0451fdb3c7d9cc7f0730c9f46209ac2
Prantik <prantiknoor@gmail.com>
2023-05-12 12:26:31 +0600

    string analyzer created

---
+++ assignments/001-rest-api-of-some-algorithms/src/algorithms/string-analyzer.js
@@ -0,0 +1,23 @@
+/**
+ * a function that can accept a string as an argument and
+ * return an object containing the number of letters, number
+ * of symbols and number of numeric characters.
+ * 
+ * @param {string} str
+ * @returns {object}
+ */
+module.exports.stringAnalyzer = (str) => {
+  const alphabets = new Set([..."abcdefghijklmnopqrstuvwxyz"]);
+  const digits = new Set([..."0123456789"]);
+  return [...str].reduce(
+    (acc, char) => {
+      if (alphabets.has(char)) acc.alphabets++;
+      else if (digits.has(char)) acc.numbers++;
+      else acc.symbols++;
+      return acc;
+    },
+    { alphabets: 0, numbers: 0, symbols: 0 }
+  );
+};
+
+console.log(this.stringAnalyzer("abcd1232#"))
