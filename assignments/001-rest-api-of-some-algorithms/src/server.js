250a21a17c93d67803f605e0107814a32a9fb70b
Prantik <prantiknoor@gmail.com>
2023-05-12 15:04:24 +0600

    Express app created

---
+++ assignments/001-rest-api-of-some-algorithms/src/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use([cors(), morgan("dev"), express.json()]);
+
+app.get("/health", (_req, res) => {
+  res.status(200).json({ message: "Success" });
+});
+
+app.use((_req, _res, next) => {
+  const err = new Error("404 Not found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((error, _req, res, _next) => {
+  if (error.status) {
+    res.status(error.status).json({ message: error.message });
+  } else {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
+});
+
+app.get("/health", (_req, res) => {
+  res.status(200).json({ message: "Success" });
+});
+
+const port = process.env.PORT || 2023;
+app.listen(port, () => {
+  console.log("App is listening on PORT", port);
+});
