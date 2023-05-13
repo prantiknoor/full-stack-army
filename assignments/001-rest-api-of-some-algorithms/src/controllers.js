d7070a5121de0a8cabc5e15342fc91a6c7bf5387
Prantik <prantiknoor@gmail.com>
2023-05-13 13:57:23 +0600

    Clean up controllers.js

--- assignments/001-rest-api-of-some-algorithms/src/controllers.js
+++ assignments/001-rest-api-of-some-algorithms/src/controllers.js
@@ -1,34 +1,81 @@
 const generateFakePerson = require("./algorithms/generate-fake-person");
 const randomNumberGenerator = require("./algorithms/random-number-generator");
 const stringAnalyzer = require("./algorithms/string-analyzer");
+const { error } = require("./utils");
+const express = require("express");
 
-module.exports.randomNumber = (req, res) => {
-  let { start, end } = req.query;
-  if (!start && !end) (start = 0), (end = 100);
-  (start = parseInt(start)), (end = parseInt(end));
-  if (isNaN(start) || isNaN(end)) {
-    return res.status(400).json({ message: "Invalid input" });
-  }
-  const number = randomNumberGenerator(start, end);
-  res.status(200).json({ number, start, end });
+/**
+ * Controller of /random-number
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+const randomNumber = (req, res) => {
+    let { start, end } = req.query;
+
+    // if both start, end are undefined, set default value
+    if (!start && !end) {
+        start = 0;
+        end = 100;
+    }
+
+    start = parseInt(start);
+    end = parseInt(end);
+
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        throw error(400, "Invalid input");
+    }
+
+    const number = randomNumberGenerator(start, end);
+
+    res.status(200).json({ number, start, end });
 };
 
-module.exports.analyzeString = (req, res) => {
-  const s = req.method === "POST" ? req.body.toString() : req.query.s;
-  if (!s) return res.status(400).json({ message: "Invalid input" });
-  const report = stringAnalyzer(s);
-  res.status(200).json({ s, report });
+/**
+ * Controller of /analyze-string
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+const analyzeString = (req, res) => {
+    let text;
+    if (req.method === "POST") {
+        text = req.body.toString();
+    } else {
+        if (!req.query.s) throw error(400, "Invalid input");
+
+        try {
+            text = decodeURIComponent(req.query.s);
+        } catch {
+            text = req.query.s;
+        }
+    }
+
+    const report = stringAnalyzer(text);
+
+    res.status(200).json({ s: text, report });
 };
 
-module.exports.getFakePerson = (req, res, next) => {
-  try {
-    let props = JSON.parse(`[${req.query.props ?? ''}]`);
-    props = props.length ? props : null;
-    const fakePerson = generateFakePerson(props);
-    res.status(200).json(fakePerson);
-  } catch {
-    const err = new Error("Invalid input");
-    err.status = 400;
-    next(err);
-  }
+/**
+ * Controller of /generate-fake-person
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+const getFakePerson = (req, res) => {
+    try {
+        let props;
+        if (req.query.props) {
+            props = JSON.parse(`[${req.query.props ?? ""}]`);
+        }
+
+        const fakePerson = generateFakePerson(props);
+
+        res.status(200).json(fakePerson);
+    } catch (err) {
+        throw err instanceof SyntaxError ? error(400, "Invalid input") : err;
+    }
+};
+
+module.exports = {
+    randomNumber,
+    analyzeString,
+    getFakePerson,
 };
