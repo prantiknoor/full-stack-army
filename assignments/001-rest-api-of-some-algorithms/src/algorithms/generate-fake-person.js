598c6ef64bf5b9274220df5b61323f8f9ba379d6
Prantik <prantiknoor@gmail.com>
2023-05-13 13:58:15 +0600

    Clean up generate-fake-person.js

--- assignments/001-rest-api-of-some-algorithms/src/algorithms/generate-fake-person.js
+++ assignments/001-rest-api-of-some-algorithms/src/algorithms/generate-fake-person.js
@@ -6,26 +6,31 @@ const randomNumberGenrator = require("./random-number-generator");
  * last name, email, avatar, age and address. User can pass an array of selected
  * properties and it will return a person object containing only given properties.
  * Ex: generatePerson([’firstName’, ‘lastName’, ‘email’])
- * 
+ *
  * @param {object} props
  * @returns {object}
  */
 module.exports = function generateFakePerson(props) {
-  const fakeDataGenerators = {
-    firstName: faker.name.firstName,
-    lastName: faker.name.lastName,
-    email: faker.internet.email,
-    avatar: faker.image.avatar,
-    age: () => randomNumberGenrator(12, 60),
-    address: () =>
-      `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.stateAbbr()}`,
-  };
-  const fakePerson = {};
-  props ??= Object.keys(fakeDataGenerators);
-  for (let prop of props) {
-    if (prop in fakeDataGenerators) {
-      fakePerson[prop] = fakeDataGenerators[prop]();
-    }
-  }
-  return fakePerson;
-}
+    const fakeDataGenerators = {
+        firstName: faker.name.firstName,
+        lastName: faker.name.lastName,
+        email: faker.internet.email,
+        avatar: faker.image.avatar,
+        age: () => randomNumberGenrator(12, 60),
+        address: () =>
+            [
+                faker.address.streetAddress(),
+                faker.address.city(),
+                faker.address.stateAbbr(),
+            ].join(", "),
+    };
+
+    props ??= Object.keys(fakeDataGenerators);
+
+    return props.reduce((fakePerson, prop) => {
+        if (prop in fakeDataGenerators) {
+            fakePerson[prop] = fakeDataGenerators[prop]();
+        }
+        return fakePerson;
+    }, {});
+};
