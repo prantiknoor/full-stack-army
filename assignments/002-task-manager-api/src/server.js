407907046245316c633e321e0d9d6386bb885168
Prantik <prantiknoor@gmail.com>
2023-05-20 17:38:32 +0600

    global middlewares & error handlers added

--- assignments/002-task-manager-api/src/server.js
+++ assignments/002-task-manager-api/src/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const { middlewares, globalErrorHandler, notFoundHandler } = require("./middlewares");
 
 const app = express();
+
+app.use(middlewares);
 
 app.get('/health', (_req, res) => {
     res.status(200).json({ status: 'OK' });
 });
+
+app.use(notFoundHandler);
+app.use(globalErrorHandler);
 
 const PORT = process.env.PORT || 2023;
 
